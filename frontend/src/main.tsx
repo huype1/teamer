2aa15b4fb7706038055674674a9942dad0e868b4
huype1 <blockerhuy7704@gmail.com>
2025-08-30 19:28:04 +0700

    remove so it will work for netlify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ if (typeof global === 'undefined') {
   (window as unknown as { global: typeof window }).global = window;
 }
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '647768355555-ofa88gojqg46s7c5ph3irvv5cpfpm8a2.apps.googleusercontent.com';
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
